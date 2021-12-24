8cc020b28c26739a93116b24f4817147c593532b
sunc-dev <sunchao_dev@163.com>
2021-12-24 12:00:21 +0800

    second commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
-
+        System.out.println("hello git!2");
     }
 }
